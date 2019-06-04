436d3fdf59b8aff31c9f096747004dc8dbe8bc4c
Joe Manthey <jdmanthey@gmail.com>
2019-06-04 12:25:24 -0500

    Create 01_delly1_hrothgar.sh

---
+++ 12_inversions/01_delly1_hrothgar.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N C_delly1
+#$ -q Chewie
+#$ -pe sm 4
+#$ -P communitycluster
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=10G
+#$ -t 1:25
+
+/home/jmanthey/delly_v0.8.1_linux_x86_64bit call -g /home/jmanthey/references/06_certhia_reordered.fasta \
+-o /lustre/scratch/jmanthey/01_certhia_genomics/09_delly/${SGE_TASK_ID}_SV.bcf \
+-t INV -q 20 -s 15 \
+-x /lustre/scratch/jmanthey/01_certhia_genomics/09_delly/exclusion_list.bed \
+/lustre/scratch/jmanthey/01_certhia_genomics/01_bam_files/${SGE_TASK_ID}_final.bam
