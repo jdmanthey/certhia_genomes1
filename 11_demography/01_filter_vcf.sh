7f80cc457bfae352c371bd2b1ff02ead4b4e38a4
Joe Manthey <jdmanthey@gmail.com>
2019-06-13 17:01:49 -0500

    Update 01_filter_vcf.sh

--- 11_demography/01_filter_vcf.sh
+++ 11_demography/01_filter_vcf.sh
@@ -11,4 +11,11 @@
 #$ -t 1-25
 
 vcftools --vcf ${SGE_TASK_ID}.rn.vcf --max-missing 0.5 --minQ 20 --minGQ 20 --minDP 5 \
---max-meanDP 60 --recode --remove-indels --keep smc_keep.txt --out ${SGE_TASK_ID}
+--max-alleles 2 --max-meanDP 60 --recode --remove-indels --keep smc_keep.txt --out ${SGE_TASK_ID}
+
+grep '^Ca' ${SGE_TASK_ID}.recode.vcf | cut -f1,2 > ${SGE_TASK_ID}.sites
+
+bgzip -c ${SGE_TASK_ID}.recode.vcf > ${SGE_TASK_ID}.recode.vcf.gz
+
+tabix -p vcf ${SGE_TASK_ID}.recode.vcf.gz
+
