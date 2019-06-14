73c2516c8c2b16fdcb5b9154401e2a0106f4d942
Joe Manthey <jdmanthey@gmail.com>
2019-06-14 15:55:58 -0500

    Create 06_test_smcpp_4.sh

---
+++ 11_demography/06_test_smcpp_4.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N test_smc
+#$ -q omni
+#$ -pe sm 12
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+
+source activate smcpp_mod
+
+# run with americana
+smc++ cv 2.506e-09 -o americana_test1 --cores 12 --knots 16 --timepoints 5e2 3e6 --regularization-penalty 15 \
+--spline cubic *americana.smc
