43cd505c134933c7f9b12277f826f69f630aa4f5
Joe Manthey <jdmanthey@gmail.com>
2019-06-17 11:53:32 -0500

    Create 07_americana_2.sh

---
+++ 11_demography/07_americana_2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N smc_ame2
+#$ -q omni
+#$ -pe sm 12
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+
+source activate smcpp_mod
+
+# run with americana
+smc++ cv 2.506e-09 -o americana_2 --cores 12 --knots 16 --timepoints 3e3 2e6 --regularization-penalty 6 \
+--spline cubic *americana.smc
