62a29bffd9cccd78d444d10dab5685858e3eb312
Joe Manthey <jdmanthey@gmail.com>
2019-06-17 11:55:55 -0500

    Create 07_albescens_2.sh

---
+++ 11_demography/07_albescens_2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N smc_alb2
+#$ -q omni
+#$ -pe sm 12
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+
+source activate smcpp_mod
+
+# run with albescens
+smc++ cv 2.506e-09 -o albescens_2 --cores 12 --knots 16 --timepoints 3e3 2e6 --regularization-penalty 10 \
+--spline cubic *albescens.smc
