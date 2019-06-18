0664072a2a556efda946314a01c32fd2860e10ad
Joe Manthey <jdmanthey@gmail.com>
2019-06-18 10:41:40 -0500

    Update 07_americana_1.sh

--- 11_demography/07_americana_1.sh
+++ 11_demography/07_americana_1.sh
@@ -2,9 +2,9 @@
 #$ -V
 #$ -cwd
 #$ -S /bin/bash
-#$ -N smc_ame1
+#$ -N smc_ame2
 #$ -q omni
-#$ -pe sm 12
+#$ -pe sm 24
 #$ -P quanah
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=8G
@@ -12,5 +12,5 @@
 source activate smcpp_mod
 
 # run with americana
-smc++ cv 2.506e-09 -o americana_1 --cores 12 --knots 16 --timepoints 3e3 2e6 --regularization-penalty 10 \
+smc++ cv 2.506e-09 -o americana_2 --cores 24 --knots 16 --timepoints 5e3 2e6 --regularization-penalty 6 \
 --spline cubic *americana.smc
