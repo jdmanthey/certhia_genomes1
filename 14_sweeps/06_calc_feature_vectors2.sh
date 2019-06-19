bfae05678f8635864d2b89dbbb020d2850fad7d4
Joe Manthey <jdmanthey@gmail.com>
2019-06-19 17:08:30 -0500

    Create 06_calc_feature_vectors2.sh

---
+++ 14_sweeps/06_calc_feature_vectors2.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N diplo_2
+#$ -q omni
+#$ -pe sm 12
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+
+source activate selection
+
+
+python ~/diploSHIC/diploSHIC.py fvecSim diploid albescens_neutral.txt.gz \
+albescens_neutral.fvec --totalPhysLen 220000 \
+--maskFileName diplo_mask.fasta.gz --chrArmsForMasking all --numSubWins 11
+
+
+python ~/diploSHIC/diploSHIC.py fvecSim diploid americana_neutral.txt.gz \
+americana_neutral.fvec --totalPhysLen 220000 \
+--maskFileName diplo_mask.fasta.gz --chrArmsForMasking all --numSubWins 11
