199316948f24313da4a9c9e45f97201810740f72
Joe Manthey <jdmanthey@gmail.com>
2019-06-20 15:37:49 -0500

    Update 08_training1.sh

--- 14_sweeps/08_training1.sh
+++ 14_sweeps/08_training1.sh
@@ -1,12 +1,8 @@
-#!/bin/sh
-#$ -V
-#$ -cwd
-#$ -S /bin/bash
-#$ -N training_1
-#$ -q omni
-#$ -pe sm 8
-#$ -P quanah
-#$ -l h_rt=48:00:00
-#$ -l h_vmem=8G
+
+cd /lustre/scratch/jmanthey/07_certhia_sim
+
+source activate selection2
 
 python ~/diploSHIC/diploSHIC.py train training_albescens/ training_albescens/ albescensModel
+
+python ~/diploSHIC/diploSHIC.py train training_americana/ training_americana/ americanaModel
