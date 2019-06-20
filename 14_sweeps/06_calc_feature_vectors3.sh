af2842dfd97b5f94a2735ce0270116db359c0633
Joe Manthey <jdmanthey@gmail.com>
2019-06-20 09:11:12 -0500

    Create 06_calc_feature_vectors3.sh

---
+++ 14_sweeps/06_calc_feature_vectors3.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N diplo_3
+#$ -q omni
+#$ -pe sm 12
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+#$ -t 1-11
+
+source activate selection
+
+revised_number="$((${SGE_TASK_ID}-1))"
+
+
+python ~/diploSHIC/diploSHIC.py fvecSim diploid albescens_soft_${revised_number}.txt.gz \
+albescens_soft_${revised_number}.fvec --totalPhysLen 220000 \
+--maskFileName diplo_mask.fasta.gz --chrArmsForMasking all --numSubWins 11
+
+
