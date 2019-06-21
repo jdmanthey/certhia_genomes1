03d5452828802da8c328ff08f7ba1bc2061d5240
Joe Manthey <jdmanthey@gmail.com>
2019-06-21 17:06:54 -0500

    Update 08_training1.sh

--- 14_sweeps/08_training1.sh
+++ 14_sweeps/08_training1.sh
@@ -1,8 +1,12 @@
 
-cd /lustre/scratch/jmanthey/07_certhia_sim
+cd /lustre/scratch/jmanthey/07b_certhia_diplo
 
 source activate selection2
 
 python ~/diploSHIC/diploSHIC.py train training_albescens/ training_albescens/ albescensModel
+diploSHIC loss: 0.762922
+diploSHIC accuracy: 0.656000
 
 python ~/diploSHIC/diploSHIC.py train training_americana/ training_americana/ americanaModel
+diploSHIC loss: 0.682005
+diploSHIC accuracy: 0.690000
