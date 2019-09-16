176723e5c585feb2bae93df3e764e56b870dd6b9
Joe Manthey <jdmanthey@gmail.com>
2019-09-16 16:18:47 -0500

    Create 10_americana_bootstraps.sh

---
+++ 11_demography/10_americana_bootstraps.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N ame_boot
+#$ -q omni
+#$ -pe sm 24
+#$ -P quanah
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=8G
+#$ -t 1-10
+
+source activate smcpp_mod
+
+# run with albescens
+smc++ cv 2.506e-09 -o boot2_${SGE_TASK_ID} --cores 24 --knots 16 --timepoints 5e3 2e6 --regularization-penalty 6 \
+--spline cubic bootstrap_${SGE_TASK_ID}/*americana.smc
