e94ab0a6f50c775b1f9688568b5aa62af21d0b5d
Joe Manthey <jdmanthey@gmail.com>
2019-09-17 10:08:37 -0500

    Create 03_raxml_array.sh

---
+++ 13_phylogenies/03_raxml_array.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N ca_raxml
+#$ -o $JOB_NAME.o$JOB_ID
+#$ -e $JOB_NAME.e$JOB_ID
+#$ -q omni
+#$ -pe sm 1
+#$ -P quanah
+#$ -l h_rt=01:00:00
+#$ -l h_vmem=8G
+#$ -t 1-18470
+
+raxmlHPC-PTHREADS-SSE3 -T 1 -f a -x 50 -m GTRGAMMA -p 253 -N 100 \
+-s /lustre/scratch/jmanthey/01_certhia_genomics/certhia_fasta/${SGE_TASK_ID}.fasta \
+-n certhia_${SGE_TASK_ID}.tre -w /lustre/scratch/jmanthey/01_certhia_genomics/certhia_fasta/
+
+rm lustre/scratch/jmanthey/01_certhia_genomics/certhia_fasta/RAxML_bestTree.certhia_${SGE_TASK_ID}.tre
+
+rm lustre/scratch/jmanthey/01_certhia_genomics/certhia_fasta/RAxML_bipartitionsBranchLabels.certhia_${SGE_TASK_ID}.tre
+
+rm lustre/scratch/jmanthey/01_certhia_genomics/certhia_fasta/RAxML_bootstrap.certhia_${SGE_TASK_ID}.tre
+
+rm lustre/scratch/jmanthey/01_certhia_genomics/certhia_fasta/RAxML_info.certhia_${SGE_TASK_ID}.tre
