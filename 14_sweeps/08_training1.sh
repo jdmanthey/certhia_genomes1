1797ebfa56f6781c90e8ee64fcd8c1d93ec0dd95
Joe Manthey <jdmanthey@gmail.com>
2019-10-01 10:18:22 -0500

    Update 08_training1.sh

--- 14_sweeps/08_training1.sh
+++ 14_sweeps/08_training1.sh
@@ -1,12 +1,20 @@
 
-cd /lustre/scratch/jmanthey/07b_certhia_diplo
+cd /lustre/scratch/jmanthey/07_certhia_sim
 
 source activate selection2
 
-python ~/diploSHIC/diploSHIC.py train training_albescens/ training_albescens/ albescensModel
-diploSHIC loss: 0.762922
-diploSHIC accuracy: 0.656000
+# run several training sessions (e.g., ~5) and choose the best training model for each species
 
-python ~/diploSHIC/diploSHIC.py train training_americana/ training_americana/ americanaModel
-diploSHIC loss: 0.682005
-diploSHIC accuracy: 0.690000
+python ~/diploSHIC/diploSHIC.py train training_albescens/ training_albescens/ albescensModel5
+diploSHIC loss: 0.739600
+diploSHIC accuracy: 0.688000
+
+python ~/diploSHIC/diploSHIC.py train training_americana/ training_americana/ americanaModel4
+diploSHIC loss: 0.647544
+diploSHIC accuracy: 0.712000
+
+mv albescensModel5.json albescensModel.json  
+mv albescensModel5.weights.hdf5 albescensModel.weights.hdf5
+
+mv americanaModel4.json americanaModel.json
+mv americanaModel4.weights.hdf5 americanaModel.weights.hdf5
