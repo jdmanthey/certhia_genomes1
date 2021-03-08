2ca88b68a1d87397021239bd76614dda2b0a1b37
Joe Manthey <jdmanthey@gmail.com>
2021-03-08 09:35:37 -0600

    Update and rename step5_find_lhood_range_best_models.sh to step5_prep_lhood_range_best_models.sh

---
+++ 16_revisions/07_fastsimcoal/step5_prep_lhood_range_best_models.sh
@@ -0,0 +1,12 @@
+cd /lustre/scratch/jmanthey/05_certhia_genomics/04_fastsimcoal/02_no_inv
+
+cd isolation_w_migration
+cp -r observed/6/isolation_w_migration best_ll
+cd ../isolation
+cp -r observed/17/isolation best_ll
+cd ../secondary_contact
+cp -r observed/15/secondary_contact best_ll
+cd ../speciation_w_geneflow
+cp -r observed/9/speciation_w_geneflow best_ll
+cd ..
+
