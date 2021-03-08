a7e2851122b7e950d593ee29d30abdc8e2a81b96
Joe Manthey <jdmanthey@gmail.com>
2021-03-08 09:32:36 -0600

    Create step2_make_directories.sh

---
+++ 16_revisions/07_fastsimcoal/step2_make_directories.sh
@@ -0,0 +1,21 @@
+# make observed output directories
+
+mkdir isolation_w_migration
+mkdir isolation
+mkdir secondary_contact
+mkdir speciation_w_geneflow
+
+
+
+cd isolation_w_migration
+mkdir observed
+cd ../isolation
+mkdir observed
+cd ../secondary_contact
+mkdir observed
+cd ../speciation_w_geneflow
+mkdir observed
+cd ..
+
+
+
